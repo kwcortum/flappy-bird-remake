dda8e29c9f68862073a03fc88e3c18d39f2cd3b9
kwcortum <96136470+kwcortum@users.noreply.github.com>
2022-01-17 21:31:35 -0600

    Deleted the jump button and added the onkeydown handler.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,4 @@
 window.onload = function() {
-    let btn = document.getElementById("jump");
     let count = 0;
 
     let canvas = document.getElementById("canvas");
@@ -11,24 +10,39 @@ window.onload = function() {
     context.arc(x, y, 50, 0, 2*Math.PI);
     context.fillStyle="red";
     context.fill();
-    
-    btn.onclick = function() {
+
+    document.onkeydown = function() {
         count += 1;
-        console.log(count);
         y -= 25;
-        console.log(y);
+        console.log(count, y);
         if (y <= -50) {
             y = 450;
           }
-        context.clearRect(0, 0, 600, 400);
-
-        context.beginPath();
-        context.arc(x, y, 50, 0, 2*Math.PI);
-        context.fillStyle="red";
-        context.fill();
-
-        context.font = '25px Arial';
-        context.fillStyle = 'white';
-        context.fillText("Count: " + count, 20, 30);
+        draw();
     }
+
+    document.ontouchstart = function() {
+        count += 1;
+        y -= 25;
+        if (y <= -50) {
+            y = 450;
+          }
+        draw();
+    }
+
+        function draw(){
+
+            context.clearRect(0, 0, 600, 400);
+
+            context.beginPath();
+            context.arc(x, y, 50, 0, 2*Math.PI);
+            context.fillStyle="red";
+            context.fill();
+
+            context.font = '25px Arial';
+            context.fillStyle = 'white';
+            context.fillText("Count: " + count, 20, 30);
+
+            window.requestAnimationFrame(draw);
+        }
 }
