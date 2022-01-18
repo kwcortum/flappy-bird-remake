27bd0a662532037efc981338b302fe82936ef0f9
kwcortum <96136470+kwcortum@users.noreply.github.com>
2022-01-17 18:39:16 -0600

    Added onclick animation and compiled shape into one window.onload function

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,20 +1,31 @@
 window.onload = function() {
     let btn = document.getElementById("jump");
     let count = 0;
-    
-    btn.onclick = function() {
-        count += 1;
-        console.log(count)
-    }
-}
 
-window.onload = function() {
     let canvas = document.getElementById("canvas");
     let context = canvas.getContext("2d");
     let x = 300;
     let y = 350;
 
+    context.beginPath();
     context.arc(x, y, 50, 0, 2*Math.PI);
     context.fillStyle="red";
     context.fill();
+    
+    btn.onclick = function() {
+        count += 1;
+        console.log(count);
+        y -= 25;
+        console.log(y);
+        context.clearRect(0, 0, 600, 400);
+
+        context.beginPath();
+        context.arc(x, y, 50, 0, 2*Math.PI);
+        context.fillStyle="red";
+        context.fill();
+
+        context.font = '25px Arial';
+        context.fillStyle = 'white';
+        context.fillText("Count: " + count, 20, 30);
+    }
 }
